a71fdabf97ebec2c9dd7ff2f2f9ac79b9594793e
曲效鹏 <quxiaopeng@haizhi.com>
2015-12-11 20:51:23 +0800

    删除不用的项目

---
+++ MyFirstApplication/hellochartstest/src/main/java/com/example/quxiaopeng/hellochartstest/LineChartActivity.java
@@ -0,0 +1,15 @@
+package com.example.quxiaopeng.hellochartstest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by quxiaopeng on 15/12/11.
+ */
+public class LineChartActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linechart);
+    }
+}
--- MyFirstApplication/hellochartstest/src/main/java/com/example/quxiaopeng/hellochartstest/MainActivity.java
+++ MyFirstApplication/hellochartstest/src/main/java/com/example/quxiaopeng/hellochartstest/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.quxiaopeng.hellochartstest;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button lineChart = (Button) findViewById(R.id.btn_linechart);
+
+        lineChart.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_linechart:
+                startActivity(new Intent(this,LineChartActivity.class));
+                break;
+            default:
+                break;
+        }
     }
 }
